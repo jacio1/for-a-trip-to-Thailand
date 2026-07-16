893692407a1516a8f3717611ce972186f668fce1
Vyacheslav <146721224+jacio1@users.noreply.github.com>
2026-07-16 12:52:29 +0300

    Update layout.tsx (#5)
    * Update layout.tsx
    
    * Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,16 +3,9 @@
 import "./global.css";
 import { I18nProvider } from "./providers/i18n-provider";
 
-<<<<<<< HEAD
 
-export const metadata: Metadata = {
-  title: "Jacio1 hardcoded",
-  description: "Redmorrayne is god tier dev",
-};
-=======
 import NavigationBar from "../components/NavigationBar";
 import { usePathname } from "next/navigation";
->>>>>>> ed5f3b9393eb4c999d518737f28a0704c49e4d5f
 
 export default function RootLayout({
   children,
@@ -28,7 +21,7 @@ export default function RootLayout({
     <html lang="en" data-theme="dark" className="w-full h-full">
       <body className="w-full h-full flex items-center justify-center bg-[#202020]">
         <div className="w-full h-full max-w-120 max-h-240 relative bg-(--page-bg) overflow-hidden">
-          <div className="h-full pb-20 overflow-y-auto">
+          <div className="h-full overflow-y-auto">
             <I18nProvider>{children}</I18nProvider>
           </div>
 
